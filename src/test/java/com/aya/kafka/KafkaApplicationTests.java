02887426ed3dd9a15f61e18542d86fe7b5122ce6
Aya Shabana <59667304+aya444@users.noreply.github.com>
2024-08-27 01:05:52 +0300

    first commit

---
+++ src/test/java/com/aya/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aya.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
